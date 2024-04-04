c59825e87925b9c2723a5719310bf1e4d7076cd2
hazzokko <hazzokko@gmail.com>
2024-04-04 16:20:11 +0900

    3. 엔티티 설계 시 주의점 (지연로딩 코드 추가)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,14 +19,14 @@ public class Order {
     @Column(name = "order_id") // id로 해도 되지만 DBA가 order_id를 더 선호해서 맞춘다
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id") // 매핑할 FK 이름 지정, 여기에 값을 세팅하면 member_id FK 값이 변경된다.
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
