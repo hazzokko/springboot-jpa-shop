62b2058d5d77e31e7ed70a4cb857df6a7e118dd7
hazzokko <hazzokko@gmail.com>
2024-04-04 18:13:14 +0900

    2. 엔티티 설계 시 주의점 (연관관계 편의 메서드 추가)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -21,17 +21,33 @@ public class Order {
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id") // 매핑할 FK 이름 지정, 여기에 값을 세팅하면 member_id FK 값이 변경된다.
-    private Member member;
+    private Member member; // 주문 회원
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
-    private Delivery delivery;
+    private Delivery delivery; // 배송 정보
 
     private LocalDateTime orderDate; // 주문 시간, Date를 사용하면 날짜 관련 어노테이션 매핑이 필요, JDK8에서는 LocalDateTime을 사용하면 하이버네이트가 자동 지원
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 -> ORDER, CANCEL
+
+    // 연관관계 편의 메서드 (양방향일 때 넣어주고, 위치는 핵심적으로 컨트롤하는 쪽에 만든다)
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this); // 다대일(Many-to-One) 관계에서 사용, 컬렉션(Member가 가지고 있는 Orders 목록)에 현재 객체를 추가
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this); // 일대다(One-to-Many)와 일대일(One-to-One) 관계에서 사용, 하나의 객체에만 값을 설정
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import static jakarta.persistence.FetchType.*;
 
 @Entity
+//@Table(name = "order_item")
 @Getter @Setter
 public class OrderItem {
 
@@ -17,11 +18,11 @@ public class OrderItem {
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
-    private Item item;
+    private Item item; // 주문 상품
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
-    private Order order;
+    private Order order; // 주문
 
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
