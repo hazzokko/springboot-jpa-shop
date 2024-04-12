13d8ff3abd4bd15e83ab1815421357ab30d3f4de
hazzokko <hazzokko@gmail.com>
2024-04-12 21:01:23 +0900

    Fix : 오타 수정 - jpql 쿼리문을 and로 연결할 때 and와 m.name 사이에 공백이 없어서 오류 발생

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -51,7 +51,7 @@ public class OrderRepository {
             } else {
                 jpql += " and";
             }
-            jpql += "m.name like :name";
+            jpql += " m.name like :name";
         }
 
         TypedQuery<Order> query = em.createQuery(jpql, Order.class)
